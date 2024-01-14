ff71ef1782c780096f5d4863a63cf0b7c63127e3
Jarel Sanders <jarelsanders1986@gmail.com>
2024-01-13 21:59:06 -0500

    updated backgrount and made changes to font throught css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,33 @@
-import { compileClassMetadata } from '@angular/compiler';
-import { Component, OnInit, Input, Output, NgModule } from '@angular/core';
-import { isConstructorDeclaration } from 'typescript';
+import { compileClassMetadata } from "@angular/compiler";
+import { Component, OnInit, Input, Output, NgModule } from "@angular/core";
+import { isConstructorDeclaration } from "typescript";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  title = 'currencyConversion';
+  title = "currencyConversion";
   onlineStatud: any;
   constructor() {}
   // showrate:boolean = false;
-  to_rate: string = '';
-  from_rate: string = '';
-  from_rate_icon: string = '';
-  to_rate_icon: string = '';
+  to_rate: string = "";
+  from_rate: string = "";
+  from_rate_icon: string = "";
+  to_rate_icon: string = "";
 
   ngOnInit(): void {}
 
   calculate = (): void => {
-    let currencyOne = document.getElementById('first-currency'); //select tag dropdown
-    let currencyTwo = document.getElementById('second-currency'); //select tag dropdown
-    let amountOne = document.getElementById('first-amount'); //from input tag
-    let amountTwo = document.getElementById('second-amount'); //to input tag
-    let rate = document.getElementById('current_rate');
-    let swap = document.getElementById('swap');
-    let from_rate_icon_ = document.getElementById('first-currency');
-    let to_rate_icon_ = document.getElementById('second-currency');
+    let currencyOne = document.getElementById("first-currency"); //select tag dropdown
+    let currencyTwo = document.getElementById("second-currency"); //select tag dropdown
+    let amountOne = document.getElementById("first-amount"); //from input tag
+    let amountTwo = document.getElementById("second-amount"); //to input tag
+    let rate = document.getElementById("current_rate");
+    let swap = document.getElementById("swap");
+    let from_rate_icon_ = document.getElementById("first-currency");
+    let to_rate_icon_ = document.getElementById("second-currency");
 
     let from_rate_icon = (<HTMLInputElement>from_rate_icon_).value;
     let to_rate_icon = (<HTMLInputElement>to_rate_icon_).value;
@@ -35,23 +35,24 @@ export class AppComponent implements OnInit {
     let second_currency = (<HTMLInputElement>currencyTwo).value;
     let final_amount = (<HTMLInputElement>amountOne).value;
     let amountTwo_ = (<HTMLInputElement>amountTwo).value;
-    if (final_amount === '') {
-      alert('Enter a value');
+    if (final_amount === "") {
+      alert("Enter a value");
     } else {
       //select tag dropdown
 
       var myHeaders = new Headers();
-      myHeaders.append('apikey', 'uTre7uxli89RppM2QV3e1uwHlvJSPZtu');
+
+      myHeaders.append("apikey", "xXma0EqQVkLcrfU49m9VxVTmSOaPdmrG");
 
       var requestOptions: any = {
-        method: 'GET',
-        redirect: 'follow',
+        method: "GET",
+        redirect: "follow",
         headers: myHeaders,
       };
-      const api_url = `https://api.apilayer.com/exchangerates_data/convert?to=${second_currency}&from=${first_currency}&amount=${final_amount}`;
+
+      const api_url = `https://api.apilayer.com/fixer/convert?to=${second_currency}&from=${first_currency}&amount=${final_amount}`;
       fetch(api_url, requestOptions)
         .then((response) => response.json())
-        // .then(result => console.log(result.query.amount, result.query.from, result.info.rate, result.query.to, result.result))
         .then((result) => {
           console.log(
             result.query.amount,
@@ -62,55 +63,55 @@ export class AppComponent implements OnInit {
           ),
             console.log(result),
             ((<HTMLInputElement>(
-              document.getElementById('second-amount')
+              document.getElementById("second-amount")
             )).value = result.result),
             // ((<HTMLInputElement>document.getElementById('from_rate')).value = first_currency),
-            ((<HTMLInputElement>document.getElementById('to_rate')).innerHTML =
+            ((<HTMLInputElement>document.getElementById("to_rate")).innerHTML =
               result.info.rate);
 
           // (<HTMLInputElement>document.getElementById('from_rate')).innerHTML = result.info.from
         })
 
-        .catch((error) => console.log('error', error));
+        .catch((error) => console.log("error", error));
 
-      ((<HTMLInputElement>document.getElementById('to_rate_icon')).innerText =
+      ((<HTMLInputElement>document.getElementById("to_rate_icon")).innerText =
         to_rate_icon),
         ((<HTMLInputElement>(
-          document.getElementById('from_rate_icon')
-        )).innerText = '1 ' + from_rate_icon);
+          document.getElementById("from_rate_icon")
+        )).innerText = "1 " + from_rate_icon);
 
       // (<HTMLInputElement>document.getElementById('second-amount')).textContent = result.query.amount;
 
-      currencyOne?.addEventListener('change', this.calculate);
-      currencyTwo?.addEventListener('change', this.calculate);
-      amountOne?.addEventListener('input', this.calculate);
+      currencyOne?.addEventListener("change", this.calculate);
+      currencyTwo?.addEventListener("change", this.calculate);
+      amountOne?.addEventListener("input", this.calculate);
     }
   };
 
   // swaps values
   switch = (): void => {
-    let a = (<HTMLInputElement>document.getElementById('first-amount')).value;
-    let b = (<HTMLInputElement>document.getElementById('second-amount')).value;
-    let c = (<HTMLInputElement>document.getElementById('to_rate')).innerHTML; // =  result.info.rate;
-    let d = (<HTMLInputElement>document.getElementById('from_rate')).innerHTML; //= result.query.from
-    let e = (<HTMLInputElement>document.getElementById('first-currency')).value;
-    let f = (<HTMLInputElement>document.getElementById('second-currency'))
+    let a = (<HTMLInputElement>document.getElementById("first-amount")).value;
+    let b = (<HTMLInputElement>document.getElementById("second-amount")).value;
+    let c = (<HTMLInputElement>document.getElementById("to_rate")).innerHTML; // =  result.info.rate;
+    let d = (<HTMLInputElement>document.getElementById("from_rate")).innerHTML; //= result.query.from
+    let e = (<HTMLInputElement>document.getElementById("first-currency")).value;
+    let f = (<HTMLInputElement>document.getElementById("second-currency"))
       .value;
-    let g = (<HTMLInputElement>document.getElementById('to_rate_icon')).value;
-    let h = (<HTMLInputElement>document.getElementById('from_rate_icon')).value;
+    let g = (<HTMLInputElement>document.getElementById("to_rate_icon")).value;
+    let h = (<HTMLInputElement>document.getElementById("from_rate_icon")).value;
 
     //swaps current values
     [a, b, c, d, e, f, g, h] = [b, a, c, d, f, e, h, g];
-    (<HTMLInputElement>document.getElementById('first-amount')).value = a; //swaps entered amount with second-amount
-    (<HTMLInputElement>document.getElementById('second-amount')).value = b; //swaps amount with entered first amount
+    (<HTMLInputElement>document.getElementById("first-amount")).value = a; //swaps entered amount with second-amount
+    (<HTMLInputElement>document.getElementById("second-amount")).value = b; //swaps amount with entered first amount
 
-    (<HTMLInputElement>document.getElementById('to_rate')).innerHTML = d + '  '; //swaps currency rate with from
-    (<HTMLInputElement>document.getElementById('from_rate')).innerHTML =
-      '1 ' + e; //swaps and set from rate to 1
+    (<HTMLInputElement>document.getElementById("to_rate")).innerHTML = d + "  "; //swaps currency rate with from
+    (<HTMLInputElement>document.getElementById("from_rate")).innerHTML =
+      "1 " + e; //swaps and set from rate to 1
 
     //swap currency option tag
-    (<HTMLInputElement>document.getElementById('second-currency')).value = f;
-    (<HTMLInputElement>document.getElementById('first-currency')).value = e;
+    (<HTMLInputElement>document.getElementById("second-currency")).value = f;
+    (<HTMLInputElement>document.getElementById("first-currency")).value = e;
 
     this.calculate();
   };
