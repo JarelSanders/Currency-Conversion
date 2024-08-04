c1e78984566049d44cc5ffb62f7b051db75af67c
Jarel Sanders <jarelsanders1986@gmail.com>
2024-08-04 17:53:22 -0400

    updated server side for API key

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,23 @@ require('dotenv').config(); // Load environment variables
 
 // Environment variables
 const API_BASE_URL = process.env.API_BASE_URL; 
-const API_KEY_NAME = process.env.API_KEY_NAME; 
 const API_KEY_VALUE = process.env.API_KEY_VALUE; 
 
 // Route to handle currency conversion
 router.get('/convert', async (req, res) => {
-    const { from, to, amount } = req.query; // Extract base and target currencies from query params
-
-    const apiUrl = `${API_BASE_URL}?from=${from}&to=${to}&amount${amount}`;
+    const { first_currency, second_currency, final_amount } = req.query; // Extract base and target currencies from query params
+    const apiUrl = `${API_BASE_URL}?to=${first_currency}&from=${second_currency}&amount=${final_amount}`;
 
     try {
-        const response = await needle('get', apiUrl, {
+        const response = await needle('GET', apiUrl, {
             headers: {
                 
-                 API_KEY_VALUE //API key value  
+                'apikey': API_KEY_VALUE, //API key value  
+                'Cache-Control': 'no-cache'
             }
         });
+
+        
 
         if (response.statusCode === 200) {
             res.json(response.body); // Send JSON response back to client
